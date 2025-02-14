0a3f4ba3f8c61b947cfbcfd09ab04a61dd1aac6f
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-02-13 22:18:03 -0600

    ADDfunction to extract data from books into GetBooks module

--- DataExtraction/GetBooks.py
+++ DataExtraction/GetBooks.py
@@ -2,18 +2,22 @@ from time import sleep
 import requests
 import json
 
-def GetBooksData(Subjects:list[str]) -> list[tuple[str,str]]:
+from typing import Iterable
+
+def GetBooksData(Subjects:list[str],AmountBooks:int) -> Iterable[list[tuple[str,str]]]:
     """
         Function for getting data of books from 
         a list of subjects
 
-        -- Subjects : list[str] :: List of subjects 
+        -- Subjects : list[str] :: List of subjects
 
-        Return a list of titles and IDs
+        -- AmountBooks : int :: Amount of books to get
+
+        Yield a list of IDs and titles
     """
     for subject in Subjects:
         books = GetTitlesBySubject(subject)
-        yield ExtractDataBooks(books)
+        yield ExtractDataBooks(books,AmountBooks)
         sleep(1.5)
 
 def GetTitlesBySubject(Subject:str) -> list[dict]:
@@ -36,3 +40,22 @@ def GetTitlesBySubject(Subject:str) -> list[dict]:
     data = json.loads(response.text)
     
     return data['works']
+
+def ExtractDataBooks(Books:list[dict],AmountBooks:int) -> list[tuple[str,str]]:
+    """
+        Function for extracting essential data from books    
+    
+        -- Books : list[dict] :: List of books data 
+
+        -- AmountBooks : int :: Limit of books from which 
+        data is extracted
+
+        Return a list of IDs and titles
+    """
+    ExtractedData = []
+
+    for index_book in range(min(len(Books),AmountBooks)):
+        book = Books[index_book]
+        ExtractedData.append((book['key'][7:],book['title']))
+    
+    return ExtractedData
