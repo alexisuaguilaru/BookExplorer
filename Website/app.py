f6d74f649031f79e0799ed40c1ec78b22a4bc9a4
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-03-10 17:51:00 -0600

    SCRUM-26 MOD at navegation style and style of save selected books

--- Website/app.py
+++ Website/app.py
@@ -1,38 +1,39 @@
-from flask import Flask , render_template , request , redirect , url_for
+from flask import Flask , render_template , redirect , url_for , request , session
 import os
 
 from GettingBooks import GetRandomBooks
 
 app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 @app.route('/')
 def index():
+    session.clear()
     return render_template('index.html')
 
 @app.route('/explorer',methods=['GET','POST'])
 def update_explorer():
-    global selected_books
-    global selections
-    if request.method == 'GET':
-        selected_books = []
-        selections = 1
-        query_books = GetRandomBooks()
-        return render_template('explorer.html',QueryBooks=query_books)
-    else:
-        if selections < 5:
+    session.setdefault('selected_books',[])
+    session.setdefault('selections', 0)
+    
+    if request.method == 'POST':
+        if session['selections'] < 4:
             book_isbn = request.form.get('book_isbn')
-            selected_books.append(book_isbn)
-            selections += 1
-            query_books = GetRandomBooks()
-            return render_template('explorer.html',QueryBooks=query_books)
+            if book_isbn:
+                session['selected_books'].append(book_isbn)
+                session['selections'] += 1
+                session.modified = True
+            return redirect(url_for('update_explorer'))
         else:
-            book_isbn = request.form.get('book_isbn')
-            selected_books.append(book_isbn)
             return redirect(url_for('show_recommendations'))
+    
+    query_books = GetRandomBooks()
+    return render_template('explorer.html',QueryBooks=query_books)
 
 @app.route('/recommendations')
 def show_recommendations():
     recommended_books = GetRandomBooks() + GetRandomBooks()[:2]
+    session.clear()
     return render_template('recommendations.html',Recommendations=recommended_books)
 
 if __name__ == '__main__':
