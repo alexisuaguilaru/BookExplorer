8673b088d9076d53fd23d405ee8e4b45eb9f68e5
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-03-25 23:08:37 -0600

    SCRUM-35 ADD wagtail project for web interface

---
+++ WebInterface/WebInterface/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-tywgc(h=af6@eh-uq=0*31_x3b56h)g^&ba9i&x&k(13puz=q@"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
