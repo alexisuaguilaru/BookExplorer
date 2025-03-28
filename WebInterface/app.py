1ae8decc092140a523a8df8e1d7d000b7f8d130a
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-03-28 12:41:56 -0600

    SCRUM-35 ADD & MOD for show recommendations of books based on last selection

--- WebInterface/app.py
+++ WebInterface/app.py
@@ -1,15 +1,18 @@
-from flask import Flask , render_template , redirect , url_for , request
+from flask import Flask , render_template , redirect , url_for , request , session
 import os
 
 from source.GetBooks import RecommendedBooks
 
 app = Flask(__name__)
+app.secret_key = os.getenv("FLASK_SECRET_KEY")
 
 @app.route('/')
 def HomePage():
     ContextVariables = {
         'Title':'Book Explorer'
     }
+
+    session.clear()
     return render_template("HomePage.html",**ContextVariables)
 
 @app.route('/BookExplorer',methods=['GET','POST'])
@@ -18,14 +21,33 @@ def BookExplorer():
         'Title':'Books Exploration'
     }
 
+    session.setdefault('selections', 0)
+    session.setdefault('last_selection', '')
+
     if request.method == 'GET':
         recommended_books = RecommendedBooks()
         return render_template("BookExplorer.html",**ContextVariables,Recommendations=recommended_books)
     
     if request.method == 'POST':
-        book_isbn = request.form.get('book_isbn')
-        recommended_books = RecommendedBooks(book_isbn)
-        return redirect(url_for("BookExplorer"))
+        if session['selections'] < 4:
+            book_isbn = request.form.get('book_isbn')
+            session['selections'] += 1
+            session['last_selection'] = book_isbn
+
+            recommended_books = RecommendedBooks(book_isbn)
+            return redirect(url_for("BookExplorer"))
+
+        else:
+            return redirect(url_for("ShowRecommendations"))
+        
+@app.route('/Recommendations')
+def ShowRecommendations():
+    ContextVariables = {
+        'Title':'Books Recommendations'
+    }
+
+    recommended_books = RecommendedBooks(session['last_selection'],False)
+    return render_template("ShowRecommendations.html",**ContextVariables,Recommendations=recommended_books)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=bool(os.getenv("DEBUG_MODE")))
--- WebInterface/source/GetBooks.py
+++ WebInterface/source/GetBooks.py
@@ -7,14 +7,23 @@ from typing import Iterable
 API = "http://RecommenderSystem:8013/{resource}?isbn={isbn}"
 resource_recommendations = "recommendations"
 resource_information = "information_book"
-def RecommendedBooks(ISBN:str='') -> Iterable[dict]:
+def RecommendedBooks(ISBN:str='',Random:bool=True) -> Iterable[dict]:
     """
         Function for getting information of 
         recommended books for a some book
 
         -- ISBN : str :: Book's ISBN whose gets its recommendations
+
+        -- Random : bool :: How to treat the recommendations
     """
     isbn_books = requests.get(API.format(resource=resource_recommendations,isbn=ISBN)).json()
-    for isbn_book in sample(isbn_books,3):
-        information_book = requests.get(API.format(resource=resource_information,isbn=isbn_book)).json()
-        yield information_book
+
+    if Random:
+        for isbn_book in sample(isbn_books,3):
+            information_book = requests.get(API.format(resource=resource_information,isbn=isbn_book)).json()
+            yield information_book
+    
+    else:
+        for isbn_book in isbn_books[:3]:
+            information_book = requests.get(API.format(resource=resource_information,isbn=isbn_book)).json()
+            yield information_book
