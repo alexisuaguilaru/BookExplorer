5a377b8c8610f392e6bcc892097a70354319903f
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-03-27 23:29:58 -0600

    SCRUM-35 ADD templates for base, home page and page for book exploration ADD routes for the previous page into app

--- WebInterface/app.py
+++ WebInterface/app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask , render_template
 import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def HomePage():
-    return ""
+    ContextVariables = {
+        'Title':'Book Explorer'
+    }
+    return render_template("HomePage.html",**ContextVariables)
+
+@app.route('/BookExplorer')
+def BookExplorer():
+    ContextVariables = {
+        'Title':'Books Exploration'
+    }
+    return render_template("BookExplorer.html",**ContextVariables)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=bool(os.getenv("DEBUG_MODE")))
