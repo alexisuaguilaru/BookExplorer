2f8a56b15bd000dab843cce6f049284996a038ef
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-03-28 12:08:52 -0600

    SCRUM-35 ADD bottom for select a book

--- WebInterface/app.py
+++ WebInterface/app.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , request
+from flask import Flask , render_template , redirect , url_for , request
 import os
 
 from source.GetBooks import RecommendedBooks
@@ -21,6 +21,11 @@ def BookExplorer():
     if request.method == 'GET':
         recommended_books = RecommendedBooks()
         return render_template("BookExplorer.html",**ContextVariables,Recommendations=recommended_books)
+    
+    if request.method == 'POST':
+        book_isbn = request.form.get('book_isbn')
+        recommended_books = RecommendedBooks(book_isbn)
+        return redirect(url_for("BookExplorer"))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=5000,debug=bool(os.getenv("DEBUG_MODE")))
