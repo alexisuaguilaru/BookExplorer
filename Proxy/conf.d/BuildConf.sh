9a4f569f739c128879e8f8db949898c5da285cf3
Alexis Aguilar <alexis.uaguilaru@gmail.com>
2025-05-31 12:38:56 -0600

    SCRUM-94 MOD at modules, some deletions of deprecated modulos for web interface and deploy

--- Proxy/conf.d/BuildConf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-envsubst '$DOMAIN_NAME' < /etc/nginx/conf.d/nginx.conf.template > /etc/nginx/conf.d/default.conf
-
-chown -R $UID:$GID /etc/nginx
-
-exec gosu $UID:$GID nginx -g 'daemon off;'
--- WebInterface/source/__init__.py
+++
@@ -1,3 +0,0 @@
-from .GetBooks import RecommendedBooks
-from .FormBookExplorer import BookSelectionForm
-from .Production import Run_gunicorn
